9815f1fcc55111e453c5304ffb89283686be6978
martypdx <marty@martypdx.com>
2018-10-03 20:24:35 -0700

    control flow examples

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello bc preppers!');
+var x = 12;
+
+// (condition that if true, code block runs)
+if(x % 2 === 0) {
+    console.log('is even');
+}
+// alternative condition when if is false
+else {
+    console.log('is odd');
+}
+
+// (init; condition; post-loop)
+for(var i = 0; i < 10; i++) {
+    var y = i * 2;
+    if(y < 10) {
+        console.log('the number doubled is', y);
+    }
+}
+
